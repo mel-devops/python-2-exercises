5012e5cae0812b16041fdec529e72a3e133730cd
Melissa Schaefer <mschaefer@xpanxion.com>
2023-06-30 16:15:45 -0400

    update

--- src/solutions.py
+++ src/solutions.py
@@ -1,5 +1,7 @@
+import inspect
 from pprint import pprint
 from WordCounter import WordCounter 
+from Invoice import Invoice
 
 def sort_people(listPeople, field, direction):
     pass #TODO:
@@ -75,4 +77,25 @@ word_counter = WordCounter(sentence)
 print(word_counter.get_word_count())    # Returns the number of all the words.
 print(word_counter.get_shortest_word()) # Returns the length of the shortest word.
 print(word_counter.get_longest_word())  # Returns the length of the longest word.
+
+#------------------------------------------------------------------------------------------>
+
+data = [
+    
+        "1, 2322, 10.00, False",
+        "2, 5435, 60.30, True",
+        "3, 3433, 15.63, False",
+        "4, 8439, 12.77, False",
+        "5, 3424, 11.34, False",
+    ]
+
+invoices = []
+
+for item in data:
+    invoice_id, user_id, amount, paid = item.split(", ")
+    invoice = Invoice(invoice_id, user_id, amount, paid)
+    invoices.append(invoice)
+    
+    print(invoices)
+        
 
